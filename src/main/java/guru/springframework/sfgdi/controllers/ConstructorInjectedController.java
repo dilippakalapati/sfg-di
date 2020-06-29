0dc16d1a1e36f8c416abcd0f9be83637099637d6
dilip <dilip.aon@gmail.com>
2020-06-30 01:04:06 +0800

    Initial Commit.

---
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController {
+
+    private final GreetingService greetingService;
+
+    @Autowired
+    //Autowired for construction injection is optional from Spring 4.2. It will work even if we won't add above annotation.
+        public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
